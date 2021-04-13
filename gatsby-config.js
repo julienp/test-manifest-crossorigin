78ef714ae25edb9947c31ba40473333770200566
Julien Poissonnier <julien@caffeine.lu>
2021-04-13 11:01:02 +0200

    Add gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,20 @@ module.exports = {
   siteMetadata: {
     title: "test-manifest-crossorigin",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Manifest Repro`,
+        short_name: `Manifest Repro`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+        crossOrigin: `anonymous`, // `use-credentials` or `anonymous`
+      },
+    },
+  ],
 };
